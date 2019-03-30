bed4bf6278bff3c78b87aca57b7f0920f7eed132
vicblacknall <victorblacknall@gmail.com>
2019-03-30 01:49:22 -0400

    start

---
+++ src/test/java/com/reaction/JavaReactor/JavaReactorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.reaction.JavaReactor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaReactorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
